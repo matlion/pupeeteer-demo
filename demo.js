e55633446aed8012a6b54be054fe7888f34cbccc
Reinsch, Markus <Markus.Reinsch@Bertelsmann.de>
2019-05-17 14:11:54 +0200

    network log

--- demo.js
+++ demo.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
 
+const VALID_STATUS = [200, 304, 206, 302, 204];
+
 class Browser {
     async prepareBrowser() {
         let browser = await puppeteer.launch({
@@ -7,13 +9,37 @@ class Browser {
             args: ['--proxy-server='],
         });
 
+
         // open chromium
         let page = await browser.newPage();
+
+        // setup network logging
+        await page.setRequestInterception(true);
+
+        page.on('request', request => {
+            request.continue(); // pass it through.
+        });
+
+        page.on('response', response => {
+            const req = response.request();
+            let status = response.status();
+            if (!Util.isStatusOk(status)) {
+                let message = 'ERROR Brow-Net: url ' + req.url() + ' status ' + status;
+                console.log(message);
+            }
+        });
 
         page.setViewport({width: 1920, height: 1200});
 
         await page.goto("https://www.softwerkskammer.org/");
     }
+}
+
+class Util {
+
+    static isStatusOk(status) {
+        return VALID_STATUS.includes(status);
+    };
 }
 
 new Browser().prepareBrowser();
